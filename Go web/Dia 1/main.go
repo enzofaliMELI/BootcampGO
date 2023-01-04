5f42036acb66383a5b8b232f914cca62713d652a
enzofaliMELI <enzo.faliveni@mercadolibre.com>
2023-01-04 12:21:41 -0300

    Dia 1 Ejercicios 1 y 2 Go web

--- Go web/Dia 1/main.go
+++ Go web/Dia 1/main.go
@@ -2,8 +2,39 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type datos struct {
+	Nombre   string
+	Apellido string
+}
+
 func main() {
-	fmt.Println()
+	// Crear un router con gin
+	router := gin.Default()
+
+	// Ejercicio 1
+	router.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	// Ejercicio 2
+	router.POST("/saludo", func(c *gin.Context) {
+
+		var d datos
+
+		if err := c.ShouldBindJSON(&d); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Error al procesar los datos de entrada"})
+			return
+		}
+
+		mensaje := fmt.Sprintf("Hola %s %s", d.Nombre, d.Apellido)
+		c.JSON(http.StatusOK, gin.H{"mensaje": mensaje})
+	})
+
+	// Corremos nuestro servidor en el puerto 8080
+	router.Run()
 }
