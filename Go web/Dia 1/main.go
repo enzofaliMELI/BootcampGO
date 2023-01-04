25a5244eb25b988a80a5124cd508d700a085485e
enzofaliMELI <enzo.faliveni@mercadolibre.com>
2023-01-04 13:06:05 -0300

    c to ctx

--- Go web/Dia 1/main.go
+++ Go web/Dia 1/main.go
@@ -17,22 +17,22 @@ func main() {
 	router := gin.Default()
 
 	// Ejercicio 1
-	router.GET("/ping", func(c *gin.Context) {
-		c.String(http.StatusOK, "pong")
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "pong")
 	})
 
 	// Ejercicio 2
-	router.POST("/saludo", func(c *gin.Context) {
+	router.POST("/saludo", func(ctx *gin.Context) {
 
 		var d datos
 
-		if err := c.ShouldBindJSON(&d); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "Error al procesar los datos de entrada"})
+		if err := ctx.ShouldBindJSON(&d); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Error al procesar los datos de entrada"})
 			return
 		}
 
 		mensaje := fmt.Sprintf("Hola %s %s", d.Nombre, d.Apellido)
-		c.JSON(http.StatusOK, gin.H{"mensaje": mensaje})
+		ctx.JSON(http.StatusOK, gin.H{"mensaje": mensaje})
 	})
 
 	// Corremos nuestro servidor en el puerto 8080
